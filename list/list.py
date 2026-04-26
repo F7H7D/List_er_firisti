8f49f8ede0ad6e3dd4ff3c683d4ace43e2a74c3a
F7H7D <hkfahad09@gmail.com>
2026-04-26 22:16:08 +0600

    Update list.py

--- list/list.py
+++ list/list.py
@@ -1,7 +1,7 @@
 #List is a container of various data types 
 #it can store string, boolian expression , integer, float etc
 
-zunaider_patri = ["Fatima","nuba","sadiya"]
+zunaider_patri = ["Fatima","tuba","sadiya"]
 
 zunaider_patri.append("khadiza")
 
